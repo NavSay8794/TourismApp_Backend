3e8b598e6594117023d346dd4d7c94f11c1847ca
Naveed Sayyed <sayyednaveed107@gmail.com>
2022-08-02 20:14:01 +0530

    userController update

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,8 +40,11 @@ export const signup = async (req,res,next)=>{
 
 export const signin = async (req,res,next)=>{
     const { email , password} = req.body
+    console.log(email)
+    console.log(password)
     try{
         const oldUser = await User.findOne({email})
+        console.log('this done')
 
         if(!oldUser){
             return res.status(404).json({
@@ -50,6 +53,7 @@ export const signin = async (req,res,next)=>{
         }
 
         const isPasswordCorrect = await bcrypt.compare(password, oldUser.password)
+        console.log('this done 2')
 
         if(!isPasswordCorrect){
             return res.status(400).json({
@@ -58,6 +62,7 @@ export const signin = async (req,res,next)=>{
         }
 
         const token = jwt.sign({email: oldUser.email, id:oldUser.id}, SECRET, {expiresIn: '1h'})
+        console.log('this done 3')
         res.status(200).json({
             message:'User Logged In Successfully',
             result: oldUser,
